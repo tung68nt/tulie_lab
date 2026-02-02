789e89a73dd5b71db6c71f1600454d27bd461e34
Tulie Lab <tung68nt@users.noreply.github.com>
2026-02-03 01:51:20 +0700

    fix(types): add simple-testimonials to SectionType definition

--- client/src/types/sections.ts
+++ client/src/types/sections.ts
@@ -1,4 +1,4 @@
-export type SectionType = 'hero' | 'stats' | 'process' | 'comparison' | 'projects' | 'testimonials' | 'cta' | 'features' | 'content' | 'instructor-grid' | 'benefits' | 'coding-methods' | 'sales-countdown' | 'upsell' | 'payment' | 'custom-html' | 'student-showcase' | 'content-block' | 'instructor-bio' | 'bonus' | 'faq' | 'history' | 'curriculum' | 'pricing' | 'calendar' | 'video' | 'video-text' | 'gallery' | 'system-courses' | 'system-shop' | 'system-blog' | 'system-combos' | 'system-instructors' | 'heading' | 'cta-box' | 'course-content';
+export type SectionType = 'hero' | 'stats' | 'process' | 'comparison' | 'projects' | 'testimonials' | 'cta' | 'features' | 'content' | 'instructor-grid' | 'benefits' | 'coding-methods' | 'sales-countdown' | 'upsell' | 'payment' | 'custom-html' | 'student-showcase' | 'content-block' | 'instructor-bio' | 'bonus' | 'faq' | 'history' | 'curriculum' | 'pricing' | 'calendar' | 'video' | 'video-text' | 'gallery' | 'system-courses' | 'system-shop' | 'system-blog' | 'system-combos' | 'system-instructors' | 'heading' | 'cta-box' | 'course-content' | 'simple-testimonials';
 
 export interface SectionButton {
     text: string;
