e144a63ca9e63db9ef8a9704ea0bb3101bf0dba8
Tulie Lab <tung68nt@users.noreply.github.com>
2026-02-10 22:30:19 +0700

    style(whiteboard): remove welcome screen instructional arrows

--- client/src/components/whiteboard/WelcomeScreen.tsx
+++ client/src/components/whiteboard/WelcomeScreen.tsx
@@ -53,44 +53,6 @@ export default function WelcomeScreen({ onStart }: WelcomeScreenProps) {
                 </button>
             </div>
 
-            {/* Instructional Arrows (Hidden on mobile) */}
-            <div className="absolute inset-0 pointer-events-none z-20 hidden lg:block overflow-hidden">
-                {/* 1. Menu Arrow (Top Left) - Points to Menu Button */}
-                <div className="absolute top-24 left-24 text-zinc-400 dark:text-zinc-500 font-['Virgil'] text-xl -rotate-2">
-                    <span className="block absolute w-60 left-8 top-12 text-left leading-tight">
-                        Menu & Options
-                    </span>
-                    <svg width="100" height="100" viewBox="0 0 100 100" className="opacity-60 absolute -top-12 -left-12">
-                        {/* Simple curved arrow pointing to top-left corner */}
-                        <path d="M50,60 Q20,50 5,5" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-                        <path d="M5,15 L5,5 L15,5" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                    </svg>
-                </div>
-
-                {/* 2. Tools Arrow (Top Center) - Points to Toolbar */}
-                <div className="absolute top-36 left-1/2 -translate-x-1/2 text-zinc-400 dark:text-zinc-500 font-['Virgil'] text-xl">
-                    <span className="block absolute w-60 -left-[5rem] top-12 text-center leading-tight">
-                        Toolbar
-                    </span>
-                    <svg width="40" height="60" viewBox="0 0 40 60" className="opacity-60 absolute left-1/2 -translate-x-1/2 -top-12">
-                        {/* Simple up arrow */}
-                        <path d="M20,50 L20,5" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-                        <path d="M10,15 L20,5 L30,15" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                    </svg>
-                </div>
-
-                {/* 3. Help Arrow (Bottom Right) - Points to Help/Shortcuts */}
-                <div className="absolute bottom-20 right-24 text-zinc-400 dark:text-zinc-500 font-['Virgil'] text-xl">
-                    <span className="block absolute w-40 -left-32 top-0 text-right leading-tight">
-                        Shortcuts
-                    </span>
-                    <svg width="80" height="60" viewBox="0 0 80 60" className="opacity-60 absolute -right-6 top-2">
-                        {/* Curve down-right */}
-                        <path d="M10,20 Q40,30 70,50" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-                        <path d="M60,50 L70,50 L70,40" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                    </svg>
-                </div>
-            </div>
         </div>
     );
 }
